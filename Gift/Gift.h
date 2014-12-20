cb423a8c6ba7d8dc8a28b00620cb7ff69ac3bb23
Brian Gesiak <bgesiak@fb.com>
2014-12-19 19:38:17 -0500

    Initial commit

---
+++ Gift/Gift.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for Gift.
+FOUNDATION_EXPORT double GiftVersionNumber;
+
+//! Project version string for Gift.
+FOUNDATION_EXPORT const unsigned char GiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gift/PublicHeader.h>
