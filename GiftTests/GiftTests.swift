1a3cf89a821fd93ddf3484a37cb6f99953c1baa8
Avrohom Katz <iambpentameter@gmail.com>
2015-01-11 22:43:18 -0500

    Generalize test directory for collaboration
    Move the test repo directory to be relative to ~

--- GiftTests/GiftTests.swift
+++ GiftTests/GiftTests.swift
@@ -4,17 +4,19 @@ import LlamaKit
 
 class GiftTests: XCTestCase {
   func testExample() {
-    let fileURL = NSURL(fileURLWithPath: "/Users/bgesiak/Desktop/RepositoryTest")
+    let desktopPath = "~/Desktop".stringByExpandingTildeInPath
+    let repoPathString = "\(desktopPath)/RepositoryTest"
+    let fileURL = NSURL(fileURLWithPath: repoPathString)
     let repository = initializeEmptyRepository(fileURL!, RepositoryOptions())
     AssertSuccess(
       repository.flatMap { $0.gitDirectoryURL }.map { $0.path! },
-      "/Users/bgesiak/Desktop/RepositoryTest/.git"
+      "\(repoPathString)/.git"
     )
 
     AssertSuccess(repository.flatMap { $0.index }.map { $0.entryCount }, 0)
 
     let quickOriginURL = NSURL(string: "https://github.com/Quick/Quick.git")
-    let quickDestURL = NSURL(fileURLWithPath: "/Users/bgesiak/Desktop/ClonedQuick")
+    let quickDestURL = NSURL(fileURLWithPath: "\(desktopPath)/ClonedQuick")
     cloneRepository(quickOriginURL!, quickDestURL!, CloneOptions())
   }
 }
