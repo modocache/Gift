881a9bf82d36b83c413e63e5a7730ce90f7cfc43
Avrohom Katz <iambpentameter@gmail.com>
2015-01-18 14:42:53 -0500

    test Repository default options

---
+++ GiftTests/Repository/RepositorySpec.swift
@@ -0,0 +1,40 @@
+import Gift
+import Quick
+import Nimble
+
+class RepositorySpec: QuickSpec {
+  override func spec() {
+    var tempDirectoryURL: NSURL!
+    beforeEach {
+      let tempDirectoryPath = NSTemporaryDirectory()
+        .stringByAppendingPathComponent("com.libgit2.gift")
+        .stringByAppendingPathComponent(NSProcessInfo.processInfo().globallyUniqueString)
+
+      var error: NSError?
+      NSFileManager.defaultManager()
+        .createDirectoryAtPath(tempDirectoryPath, withIntermediateDirectories: true, attributes: nil, error: &error)
+      expect(error).to(beNil())
+
+      tempDirectoryURL = NSURL(fileURLWithPath: tempDirectoryPath)!
+    }
+
+    describe("initializeEmptyRepository") {
+      context("with default settings") {
+        beforeEach {
+          // TODO: Removing this initialization (unused) causes references to
+          //       RepositoryInitializationOptions() to segfault.
+          let _ = RepositoryInitializationOptions(
+            optionsSet: RepositoryInitializationOptionSet.MakePath,
+            mode: .User
+          )
+        }
+
+        it("initializes a repository with a working directory") {
+          let newRepoURL = tempDirectoryURL.URLByAppendingPathComponent("greenpoint")
+          let repository = initializeEmptyRepository(newRepoURL, RepositoryInitializationOptions())
+          expect(repository.isSuccess).to(beTrue())
+        }
+      }
+    }
+  }
+}
