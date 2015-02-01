a4cdd2142cb171d6cdf9fb4cf1d795a25632681d
Brian Gesiak <bgesiak@fb.com>
2015-02-01 17:21:28 -0500

    StatusDeltas: Convert to struct
    More progress on issue #8.

--- Gift/Status/StatusDelta/StatusDeltas.swift
+++ Gift/Status/StatusDelta/StatusDeltas.swift
@@ -1,11 +1,8 @@
 /**
   A pair of status deltas used to represent the deltas between
   HEAD and the index, and between the index and the working directory.
-
-  TODO: This is a class purely in order to comply to RAC 2.x APIs.
-        It should be made a struct once Gift adopts RAC 3.0.
 */
-public class StatusDeltas {
+public struct StatusDeltas {
   /**
     The status delta between HEAD and the index.
     If there is no delta, and `StatusDeltaBehavior.IncludeUnmodified`
